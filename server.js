246ed1f61ddd2f57badc968b79814590d6995481
Thomas Greco <thomasjosephgreco@yahoo.com>
2018-02-10 23:17:39 -0500

    fixed formatting in server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const next = require('next')
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const express = require('express');
+const next = require('next');
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -13,19 +13,19 @@ app.prepare().then(() => {
     server.get('/', (req, res) => res.redirect(301, '/blog'));
 
     server.get('/blog/:id', (req, res) => {
-        return app.render(req, res, '/post', Object.assign({id: req.params.id}, req.query))
+        return app.render(req, res, '/post', Object.assign({id: req.params.id}, req.query));
     });
 
     server.get('/post', (req, res) => {
-        if(req.query.id) return res.redirect(`/blog/${req.query.id}`);
+        if (req.query.id) return res.redirect(`/blog/${req.query.id}`);
         res.redirect(301, '/blog');
     });
 
     server.get('/*', (req, res) => handle(req, res));
 
     server.listen(port, err => {
-        if(err) throw err
+        if (err) throw err;
         console.log(`> Read on http://localhost:${port}`);
-    })
+    });
 });
 
